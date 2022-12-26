6f803e610211f0e6f676c1062df7ff06287ea05c
ParkBG <bgpark950821@gmail.com>
2022-12-26 15:17:01 +0900

    20221226 || update

---
+++ javascriptdemo/js/js_034_dom.js
@@ -0,0 +1,13 @@
+//js_034_dom.js
+
+let hNode = document.getElementById('selector');
+console.log(hNode);
+console.log(hNode, typeof hNode);
+
+//id
+let hId = document.querySelector('#selector');
+console.log(hId, typeof hId);
+
+//class
+let hClass = document.querySelector('.class');
+console.log(hClass, typeof hClass);
