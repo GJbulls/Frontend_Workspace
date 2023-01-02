7cb7329a86a4f99e2a3c7cfd72b5297371593cff
ParkBG <bgpark950821@gmail.com>
2023-01-02 15:19:33 +0900

    20230102 || Add data

--- javascriptdemo/my_html/js/b.js
+++
@@ -1,7 +0,0 @@
-// window.onload = function () {
-//   let wrap = document.getElementById('wrap');
-//   console.log(wrap.innerText);
-// };
-
-let wrap = document.getElementById('wrap');
-console.log(wrap.innerText);
